c61b8b3cf0eb4f1d9d6df2484511e46821daed74
Noah Z H Huang <112538853@qq.com>
2017-10-24 08:53:05 +0800

    extract the result map Address out for reusing

--- mybatis-learning/src/main/java/com/noah/mappers/StudentMapper.java
+++ mybatis-learning/src/main/java/com/noah/mappers/StudentMapper.java
@@ -2,11 +2,13 @@ package com.noah.mappers;
 
 import java.util.List;
 
+import com.noah.domain.Address;
 import com.noah.domain.Student;
 
 public interface StudentMapper {
 
 	List<Student> findAllStudents();
 	Student findStudentWIthAddressById(Integer id);
+	Address findAddressById(Integer id);
 	
 }
